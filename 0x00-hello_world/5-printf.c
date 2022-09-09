7b92e430942f91b2aaa283db5ae5e578909a8a78
Gomolemo <gomolizandrea@gmail.com>
2022-09-08 20:35:05 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - Prints a text
+ * main _ print the string in the put function
  *
- * Return: Always 0 (Successful)
+ * Description: using the function
+ * this program prints "Programming is like building a multilingual puzzle"
+ * Return: 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+}
