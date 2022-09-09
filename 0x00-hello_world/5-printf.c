f7cbb86cf231d0737e25fbce2b808a90775d251e
Gomolemo <gomolizandrea@gmail.com>
2022-09-08 20:38:19 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-
 /**
- * main _ print the string in the put function
+ * main - print the string in the put function
  *
  * Description: using the function
  * this program prints "Programming is like building a multilingual puzzle"
