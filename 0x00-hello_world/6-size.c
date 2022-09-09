1757e0a9eb234fccd51f39023665fc57c29c5a6b
Gomolemo <gomolizandrea@gmail.com>
2022-09-08 21:07:19 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,8 +2,6 @@
 /**
 * main - print the string in the put function
 *
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle"
 * Return: 0
 */
 int main(void)
