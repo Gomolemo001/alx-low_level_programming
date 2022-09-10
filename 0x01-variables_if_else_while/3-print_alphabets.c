743610eb1a0def98ee376052869e63844c52f81d
Gomolemo <gomolizandrea@gmail.com>
2022-09-09 20:00:13 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+ * main - print
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ; CH <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
