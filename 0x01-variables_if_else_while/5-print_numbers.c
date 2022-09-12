872d49127d8d511951cea0310fe65e4f2cb72e7f
Gomolemo <gomolizandrea@gmail.com>
2022-09-12 13:08:57 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,20 +2,19 @@
 /**
  * main - Entry point
  *
+ * This program prints "Programming is positive, zero or negative"
  * Return: 0
  */
 int main(void)
-
 {
 	int i;
-
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++);
 
 	{
 		printf("%i", i);
 	}
 
-	printf("\n")
+	printf("\n");
 
 		return (0);
 }
