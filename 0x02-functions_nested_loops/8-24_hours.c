aa6bdf910685ea2444c4f3fc0f33d7c12e4914a2
Gomolemo <gomolizandrea@gmail.com>
2022-09-14 10:13:50 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 
 /**
  * jack_bauer - function that proints every minute of the day of Jack Bauer,
- * starting from 00:00 to 23:59, min loop counts * mins, while hour loop counts hours
+ * from 00:00 to 23:59, min loop counts * mins, while hour loop counts hours
  * and resets mins
  * Return: 0
  */
@@ -15,19 +15,19 @@ void jack_bauer(void)
 
 	while (hours <= 23)
 	{
-	while (minutes <= 59)
-	{
-	mins_remainder = minutes % 10;
-	hours_remainder = hours % 10;
-	_putchar(hours / 10 + '0');
-	_putchar(hours_remainder + '0');
-	_putchar(':');
-	_putchar(minutes / 10 + '0');
-	_putchar(mins_remainder + '0');
-	minutes++;
-	_putchar('\n');
-	}
-	hours++;
-	minutes = 0;
-	}
+		while (minutes <= 59)
+		{
+		mins_remainder = minutes % 10;
+		hours_remainder = hours % 10;
+		_putchar(hours / 10 + '0');
+		_putchar(hours_remainder + '0');
+		_putchar(':');
+		_putchar(minutes / 10 + '0');
+		_putchar(mins_remainder + '0');
+		minutes++;
+		_putchar('\n');
+		}
+		hours++;
+		minutes = 0;
+	        }
 }
