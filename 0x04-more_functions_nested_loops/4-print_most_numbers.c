780ad72f87d5ed60cbc283115ad6706f46cb0936
Gomolemo <gomolizandrea@gmail.com>
2022-09-15 18:52:27 -0700

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,13 +10,10 @@ void print_most_numbers(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
+	for (n = 0; n < 10; n++)
 	{
-		if ((n == 50) || (n = = 52))
-		{
-			continue;
-		}
-		putchar(n);
+		if (x !=2 && x !=4)
+		putchar(x + '0');
 	}
 	_putchar('\n');
 }
