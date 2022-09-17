9252dfed05c0d14d2efa94b6e0c1cfd0d0ce3135
Gomolemo <gomolizandrea@gmail.com>
2022-09-17 02:39:19 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,27 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * more_numbers - printing more numbers
+ * more_numbers - prints numbers to 14
+ *
+ * Return: returns nothing
  */
+
 void more_numbers(void)
 {
 	int x, y;
 
 	for (x = 0; x < 10; x++)
 	{
-		for (y = 0; y < 15; y++)
+		for (y = 0; y <= 14; y++)
 		{
-			if (y >= 10)
+			if (y > 9)
+			{
 				putchar((y / 10) + '0');
+			}
 			putchar((y % 10) + '0');
 		}
-		putchar('\n');
+		putchar(10);
 	}
 }
 
