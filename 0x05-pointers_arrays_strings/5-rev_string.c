c7bfaed2288a9362dce09185bb7d9bd64f871137
Gomolemo <gomolizandrea@gmail.com>
2022-09-19 13:20:00 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * rev_string - prints a functions that reverses a string
+ * @s: string in reverse
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	char *s;
