5fb657783de556082a3daf0655bd6f6b0754cc0e
Gomolemo <gomolizandrea@gmail.com>
2022-09-19 17:36:41 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -4,15 +4,15 @@
 /**
  * puts_half - print half of a string
  * @str: the string
- * Return: nothing
+ * Return: none
  */
 
 void puts_half(char *str)
 {
 	int i, len = _strlen(str);
 
-	fot (i = ((len - 1) / 2) + 1; i < len; i++)
-		putchar(*)str + i));
+	for (i = ((len - 1) / 2) + 1; i < len; i++)
+		putchar(*(str + i));
 	putchar(10);
 }
 
