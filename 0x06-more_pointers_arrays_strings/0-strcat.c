1fcf0191a80663927a6c4f3ab8dae52006bed87b
Gomolemo <gomolizandrea@gmail.com>
2022-09-22 02:39:58 -0700

    Task 0

--- 0x06-more_pointers_arrays_strings/0-strcat.c
+++ 0x06-more_pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,12 @@
 #include "main.h"
 
 /**
- * _strcat - appends the @src string to the @dest string
- * overwriting the terminating null byte (\0)
- * at the end of @dest
- * and then adds a terminating null byte
+ * _strcat - concatenates the string pointed to by @src, including the terminat null byte, to the end of the string pointed to by @dest
  * @dest: String that will be appended
  * @src: String to be concatenated upon
  *
  * Return: returns pointer to @dest
  */
-
 char *_strcat(char *dest, char *src);
 {
 	int index = 0, dest_len = 0;
@@ -18,7 +14,7 @@ char *_strcat(char *dest, char *src);
 	while (dest[index++])
 		dest_len++;
 
-	for (indexex = 0; src[index]; index++)
+	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
 
 	return (dest);
