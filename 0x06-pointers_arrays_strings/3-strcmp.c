5d569ad7b0abd174323f1b05a7c5d5e3df301005
Gomolemo <gomolizandrea@gmail.com>
2022-09-22 09:17:11 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s1: A pointer to be compared to the first string
  * @s2: A pointer to be compared to the second string
  *
- * Return: if str1 < str2, the negative difference of the 1st unmatched char
+ * Return: if str1 < str2, the negative difference of the first unmatched char
  *        if str1 == str2, 0
  *        if str1 > str2, the positive difference of the first unmatched char
  */
