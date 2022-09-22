55da120f961caaddd9eef351ca4d3a2c43428c8f
Gomolemo <gomolizandrea@gmail.com>
2022-09-22 08:03:44 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * strcat - Concatenates the string printed to by @src,
- * the terminat null byte included, to the end
- * of the string pointed to by @dest
+ * strcat - Concatenates the string printed to by @src, terminat
+ *          null byte included, to the end of the string pointedto by @dest
  * @dest: A pointer to the string to be concatenated upon
  * @src: The source string to be appended  to @dest
  *
