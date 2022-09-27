0bc8c1e3f8ca2ebdfe2384150e47faa9ee0e962b
Gomolemo <gomolizandrea@gmail.com>
2022-09-26 17:13:23 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,14 @@
  * description _memset: over there
  *
  * Return: A pointer to the filled memory area @s.
+ *
  */
+
+char *_memset(char *s, char b, unsigned int n)
 {
 	while (n)
 	{
 		s[n - 1] = b;
 		n++;
 	}
-	return (s);
 }
