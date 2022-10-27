b576e7b953784ea7adc1657da384479435977c00
Gomolemo <gomolizandrea@gmail.com>
2022-10-27 10:45:28 -0700

    Task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,9 +3,9 @@
 /**
  * set_bit - Sets the value of a bit at a given index to 1.
  * @n: A pointer to the bit.
- * @index: The index to set the vale at - indices start at 0.
+ * @index: The index to set the value at - indices start at 0.
  *
- * Return: If an error occur - -1.
+ * Return: If an error occurs - -1.
  *	Otherwise - 1.
  */
 
